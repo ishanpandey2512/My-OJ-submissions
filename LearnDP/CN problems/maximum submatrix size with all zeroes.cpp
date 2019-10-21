ad538ffc21d08182c0d91784146b411ed062e670
Ishan Pandey <Ishan.parasar.3359@gmail.com>
2019-10-21 20:30:59 +0530

    Update maximum submatrix size with all zeroes.cpp

--- LearnDP/CN problems/maximum submatrix size with all zeroes.cpp
+++ LearnDP/CN problems/maximum submatrix size with all zeroes.cpp
@@ -1,6 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//cn way
+
+int min(int a, int b){
+    if(a>b)
+        return b;
+    else    
+        return a;
+}
+
+
+int findMaxSquareWithAllZeros(int** arr, int row, int col){
+    int dp[row][col];
+    
+    for(int i=0;i<row;i++){
+        dp[i][0] = 1-arr[i][0];
+    }
+    for(int j=0;j<col;j++){
+        dp[0][j] = 1-arr[0][j];
+    }
+    
+    for(int i=1;i<row;i++){
+        for(int j=1;j<col;j++){
+            if(arr[i][j])
+                dp[i][j] = 0;
+            else
+                dp[i][j] = min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1])) + 1;
+        }
+    }
+    
+    int m = -1;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(dp[i][j] > m)
+                m = dp[i][j];
+        }
+    }
+    
+    return m;
+    
+}
+
+//way 2
+
 int calSquare(int **arr,int m,int n)
 {
     int **dp=new int*[m];
